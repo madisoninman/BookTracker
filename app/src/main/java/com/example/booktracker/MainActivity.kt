bc513ef58ca9cc79f8abd8a8c3b76b7e67f5d8d6
Madison Inman <madisonginman@gmail.com>
2022-04-30 16:10:05 -0500

    setup recyclerview

--- app/src/main/java/com/example/booktracker/MainActivity.kt
+++ app/src/main/java/com/example/booktracker/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.example.booktracker
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.booktracker.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MyAdapter
+    //private val books = mutableListOf<Book>()
+    private val books = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // recyclerview
+        val layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerview.layoutManager = layoutManager
+        val dividerItemDecoration =
+            DividerItemDecoration(applicationContext, layoutManager.orientation)
+        binding.mainRecyclerview.addItemDecoration(dividerItemDecoration)
+        adapter = MyAdapter()
+        binding.mainRecyclerview.adapter = adapter
+    }
+
+    // RECYCLERVIEW
+    inner class MyViewHolder(val view: TextView) :
+        RecyclerView.ViewHolder(view),
+        View.OnClickListener, View.OnLongClickListener {
+
+        init {
+            view.setOnClickListener(this)
+            view.setOnLongClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+
+        }
+
+        override fun onLongClick(view: View?): Boolean {
+            return true
+        }
+    }
+
+    inner class MyAdapter :
+        RecyclerView.Adapter<MyViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_view, parent, false) as TextView
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.view.text = books[position].toString()
+        }
+
+        override fun getItemCount(): Int {
+            return books.size
+        }
     }
 }
